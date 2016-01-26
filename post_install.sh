2387a6f3ac45a004b4063c5b8525f8a0d2e7b0e8
Joe Sustaric <joe8307@gmail.com>
2016-01-27 09:50:11 +1100

    adding patch and fixing the time

--- post_install.sh
+++ post_install.sh
@@ -2,7 +2,7 @@
 
 #This is configuration needed for Golang and Chruby for both zsh and bash
 
-# ZSH ------------------------------------------------------
+# ZSH --------------------------------------------------------------------------
 # Configure zsh
 sudo -u vagrant zsh <<'EOF'
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
@@ -13,25 +13,28 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
 done
 EOF
 chsh -s /bin/zsh vagrant
-#----------------------------------------------------------
+#-------------------------------------------------------------------------------
 
-#Golang----------------------------------------------------
+#Golang-------------------------------------------------------------------------
 mkdir /vagrant/projects/go_code
 GOPATH=/vagrant/projects/go_code
 GOROOT=/usr/local/go
+#-------------------------------------------------------------------------------
+
 echo "#GOPATH needed for golang workspace" >> /home/vagrant/.bash_profile
 echo "export GOPATH=$GOPATH" >> /home/vagrant/.bash_profile
 echo "export GOROOT=$GOROOT" >> /home/vagrant/.bash_profile
 echo "export PATH=$PATH:$GOPATH/bin" >> /home/vagrant/.bash_profile
+
 if [ -f /home/vagrant/.zshrc ]; then
   echo "#GOPATH needed for golang workspace" >> /home/vagrant/.zshrc
   echo "export GOPATH=$GOPATH" >> /home/vagrant/.zshrc
   echo "export GOROOT=$GOROOT" >> /home/vagrant/.zshrc
   echo "export PATH=$PATH:$GOROOT/bin:$GOPATH/bin" >> /home/vagrant/.zshrc
 fi
-#-----------------------------------------------------------
+#-------------------------------------------------------------------------------
 
-#Chruby Config---------------------------------------------
+#Chruby Config------------------------------------------------------------------
 mkdir /vagrant/projects/ruby
 echo "#--Chruby--------------------------------" >> /home/vagrant/.bash_profile
 echo "source /usr/local/share/chruby/chruby.sh" >> /home/vagrant/.bash_profile
@@ -44,4 +47,11 @@ if [ -f /home/vagrant/.zshrc ]; then
   echo "source /usr/local/share/chruby/auto.sh" >> /home/vagrant/.zshrc
   echo "#----------------------------------------" >> /home/vagrant/.zshrc
 fi
-#--------------------------------------------------------------
+#-------------------------------------------------------------------------------
+
+#Change to any ruby version-----------------------------------------------------
+echo "chruby ruby" >> /home/vagrant/.bash_profile
+if [ -f /home/vagrant/.zshrc ]; then
+    echo "#chruby ruby" >> /home/vagrant/.zshrc
+fi
+#-------------------------------------------------------------------------------
